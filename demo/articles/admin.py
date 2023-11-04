7f81f3976122f03ef1d7856e09ef04c6e2004bde
Daikoderu <daikoderu@gmail.com>
2023-11-04 14:22:24 +0100

    add is_instance_locked and get_locked_queryset

--- demo/articles/admin.py
+++ demo/articles/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 from django.utils.safestring import SafeString
 from django_object_lock.admin import LockableAdminMixin
 
@@ -8,15 +9,29 @@ from articles.models import Article, ArticleSection
 @admin.register(Article)
 class ArticleAdmin(LockableAdminMixin):
     list_display = ('locked_icon', 'title',)
+    list_display_links = ('title',)
     fields = ('title', 'rendered_content')
     readonly_fields = ('rendered_content',)
 
     def rendered_content(self, obj: Article) -> SafeString:
         return obj.rendered_content
+    
+    def is_instance_locked(self, obj: Article) -> bool:
+        return obj.is_locked
+
+    def get_locked_queryset(self, queryset: QuerySet) -> QuerySet:
+        return queryset.filter(is_locked=True)
 
 
 @admin.register(ArticleSection)
 class ArticleSectionAdmin(LockableAdminMixin):
     list_display = ('locked_icon', 'heading', 'parent', 'order')
+    list_display_links = ('heading',)
     fields = ('parent', 'heading', 'content', 'order')
     list_select_related = ('parent',)
+
+    def is_instance_locked(self, obj: Article) -> bool:
+        return obj.parent.is_locked
+
+    def get_locked_queryset(self, queryset: QuerySet) -> QuerySet:
+        return queryset.filter(parent__is_locked=True)
--- src/django_object_lock/admin.py
+++ src/django_object_lock/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.db import models
+from django.db.models import QuerySet
+from django.http.request import HttpRequest
 from django.templatetags.static import static
 from django.utils.html import format_html
-from django.utils.safestring import SafeString
+from django.utils.safestring import SafeString, mark_safe
 from django.utils.translation import gettext_lazy as _
 
 
 class LockableAdminMixin(admin.ModelAdmin):
     """Mixin to allow model instances to be locked from the admin.
+
+    Implement ``is_instance_locked`` and ``get_locked_queryset`` to define what condition must be
+    satisfied for an instance to be locked. For example, check whether a Boolean field is set to
+    true (to let the user lock and unlock the instance manually) or depend on the "locked" status
+    of a related instance.
 
     Add the ``locked_icon`` field to ``list_display`` to display a "locked" icon in the changelist.
 
@@ -21,6 +28,25 @@ class LockableAdminMixin(admin.ModelAdmin):
         return format_html(
             format_string='<img src="{src}" alt="{alt}" />',
             src=static(self.locked_icon_static_url), alt=_('Locked')
-        )
+        ) if self.is_instance_locked(obj) else mark_safe('')
     
     locked_icon.short_description = ''
+
+    def is_instance_locked(self, obj: models.Model) -> bool:
+        """Implement this method returning ``True`` if the instance should be considered locked,
+        and ``False`` otherwise.
+        """
+        raise NotImplementedError('This method must be implemented.')
+
+    def get_locked_queryset(self, queryset: QuerySet) -> QuerySet:
+        """Implement this method returning a ``QuerySet`` that returns only locked objects.
+        """
+        raise NotImplementedError('This method must be implemented.')
+    
+    def has_change_permission(self, request: HttpRequest, obj: models.Model = None) -> bool:
+        is_locked = obj is not None and self.is_instance_locked(obj)
+        return not is_locked and super().has_change_permission(request, obj)
+    
+    def has_delete_permission(self, request: HttpRequest, obj: models.Model = None) -> bool:
+        is_locked = obj is not None and self.is_instance_locked(obj)
+        return not is_locked and super().has_delete_permission(request, obj)
