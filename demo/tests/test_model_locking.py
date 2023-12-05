298be256ce119a3405a4669612e5b7090e63d69d
Daikoderu <daikoderu@gmail.com>
2023-12-05 22:16:01 +0100

    simplify test data

--- demo/tests/test_model_locking.py
+++ demo/tests/test_model_locking.py
@@ -6,34 +6,28 @@ from articles.models import Article, ArticleSection
 
 class ModelLockingTestCase(TestCase):
 
-    def setUp(self) -> None:
+    @classmethod
+    def setUpTestData(cls) -> None:
         articles = [
             Article(title='Article 1', is_locked_flag=False),
             Article(title='Article 2', is_locked_flag=True),
             Article(title='Article 3', is_locked_flag=True),
             Article(title='Article 4', is_locked_flag=False),
-            Article(title='Article 5', is_locked_flag=False),
-            Article(title='Article 6', is_locked_flag=True),
+            Article(title='Article 5', is_locked_flag=True),
         ]
         Article.objects.bulk_create(articles)
 
         article_sections = [
             ArticleSection(parent=articles[0], heading='Section 1.1', content='Lorem', order=1),
-            ArticleSection(parent=articles[0], heading='Section 1.2', content='Ipsum', order=2),
             ArticleSection(parent=articles[1], heading='Section 2.1', content='Dolor', order=1),
-            ArticleSection(parent=articles[1], heading='Section 2.2', content='Sit', order=2),
-            ArticleSection(parent=articles[2], heading='Section 3.1', content='Amet', order=1),
-            ArticleSection(parent=articles[2], heading='Section 3.2', content='Consectetur', order=2),
-            ArticleSection(parent=articles[3], heading='Section 4.1', content='Adipiscing', order=1),
-            ArticleSection(parent=articles[3], heading='Section 4.2', content='Elit', order=2),
         ]
         ArticleSection.objects.bulk_create(article_sections)
 
     def test_unlocked_instance_can_be_saved(self) -> None:
-        article = Article.objects.get(pk=4)
-        article.title = 'Article 4 Edited'
+        article = Article.objects.get(pk=1)
+        article.title = 'Article 1 Edited'
         article.save()
-        self.assertEqual(Article.objects.get(pk=4).title, 'Article 4 Edited')
+        self.assertEqual(Article.objects.get(pk=1).title, 'Article 1 Edited')
 
     def test_locked_instance_cannot_be_saved(self) -> None:
         article = Article.objects.get(pk=2)
@@ -50,25 +44,25 @@ class ModelLockingTestCase(TestCase):
         self.assertEqual(Article.objects.get(pk=3).title, 'Article 3 Edited')
 
     def test_related_locked_instance_cannot_be_saved(self) -> None:
-        article_section = ArticleSection.objects.get(pk=3)
+        article_section = ArticleSection.objects.get(pk=2)
         with self.assertRaises(ObjectLocked):
             article_section.heading = 'Section 2.1 Edited'
             article_section.save()
-        self.assertEqual(ArticleSection.objects.get(pk=3).heading, 'Section 2.1')
+        self.assertEqual(ArticleSection.objects.get(pk=2).heading, 'Section 2.1')
 
     def test_related_unlocked_instance_can_be_saved(self) -> None:
-        article_section = ArticleSection.objects.get(pk=7)
-        article_section.heading = 'Section 4.1 Edited'
+        article_section = ArticleSection.objects.get(pk=1)
+        article_section.heading = 'Section 1.1 Edited'
         article_section.save()
-        self.assertEqual(ArticleSection.objects.get(pk=7).heading, 'Section 4.1 Edited')
+        self.assertEqual(ArticleSection.objects.get(pk=1).heading, 'Section 1.1 Edited')
 
     def test_unlocked_instance_can_be_deleted(self) -> None:
-        article = Article.objects.get(pk=5)
+        article = Article.objects.get(pk=4)
         article.delete()
         self.assertEqual(article.pk, None)
 
-    def test_unlocked_instance_cannot_be_deleted(self) -> None:
-        article = Article.objects.get(pk=6)
+    def test_locked_instance_cannot_be_deleted(self) -> None:
+        article = Article.objects.get(pk=5)
         with self.assertRaises(ObjectLocked):
             article.delete()
-        self.assertEqual(article.pk, 6)
+        self.assertEqual(article.pk, 5)
