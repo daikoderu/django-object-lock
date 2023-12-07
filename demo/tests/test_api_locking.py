25c9a60bf970f346632f63f9222afc1726f8c290
Daikoderu <daikoderu@gmail.com>
2023-12-07 14:09:33 +0100

    add file for API locking testing

---
+++ demo/tests/test_api_locking.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+
+from articles.models import Article, ArticleSection
+
+
+class AdminLockingTestCase(TestCase):
+    factory = APIRequestFactory()
+    articles = [
+        Article(title='Article 1', is_locked_flag=False),
+        Article(title='Article 2', is_locked_flag=True),
+        Article(title='Article 3', is_locked_flag=True),
+        Article(title='Article 4', is_locked_flag=False),
+        Article(title='Article 5', is_locked_flag=True),
+    ]
+    article_sections = [
+        ArticleSection(parent=articles[0], heading='Section 1.1', content='Lorem', order=1),
+        ArticleSection(parent=articles[1], heading='Section 2.1', content='Dolor', order=1),
+    ]
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        Article.objects.bulk_create(cls.articles)
+        ArticleSection.objects.bulk_create(cls.article_sections)
+        cls.user = User.objects.create_superuser('foo', 'foo@example.com', '123')
